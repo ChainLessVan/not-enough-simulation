27e3974b2c99469dccca144b87621e35c1350913
abathroomducky <hayes.lai.ph@gmail.com>
2026-09-15 18:50:29 +0000

    css typo js back button from setttings

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ function opengame() {
     document.getElementById("main_menu").style.display = "none";
     document.getElementById("gameArea").style.display = "block";
 
-    fetch("Game.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("gameArea").innerHTML = html;
-
-            document.getElementById("SettingsToMain").addEventListener("click", () => {
-                console.log("Back to main menu from settings");
-            });
-        });
+    document.getElementById("gameArea").innerHTML = `
+    <h2>this is game </h2>
+    <p> just test </p>
+    `;
 }
 
 function opensettings() {
     document.getElementById("main_menu").style.display = "none";
     document.getElementById("settings").style.display = "block";
 
-    document.getElementById("settings").innerHTML = `
-    <h2>this is settings </h2>
-    <p> just test </p>
-    `;
+
+    fetch("settings.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("settings").innerHTML = html;
+
+            document.getElementById("SettingsToMain").addEventListener("click", () => {
+                document.getElementById("settings").style.display = "none";
+                document.getElementById("main_menu").style.display = "block";
+                console.log("Back to main menu from settings");
+            });
+        });
 }
