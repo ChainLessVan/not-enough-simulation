c58bf27a634d4a88f273ef5e2c6b52dea8f6f2dc
abathroomducky <hayes.lai.ph@gmail.com>
2026-09-15 18:01:35 +0000

    play button

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function openpage() {
+    document.getElementById("main_menu").style.display = "none";
+    document.getElementById("gameArea").style.display = "block";
+
+    document.getElementById("gameArea").innerHTML = `
+    <h2> Lets get started then</h2>
+    <p> This is where i prove YOU scroll too much</p>
+    `;
+}
