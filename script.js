d12bb3918b0b316fa62cd948133ac37c7b895282
abathroomducky <hayes.lai.ph@gmail.com>
2026-09-16 18:21:46 +0000

    buttons

--- script.js
+++ script.js
@@ -6,31 +6,35 @@ function opengames() {
 }
 
 function opentyperace() {
+    music.pause();
     alert("WARNING: This game may not be suitable for people with epilepsy.")
-    document.getElementById("main_menu").style.display = "none";
-    document.getElementById("gameArea").style.display = "block";
+    document.getElementById("gameMenu").style.display = "none";
+    document.getElementById("speedtype").style.display = "block";
 }
 
 function opensettings() {
     document.getElementById("main_menu").style.display = "none";
     document.getElementById("settings").style.display = "block";
-
-
-    fetch("settings.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("settings").innerHTML = html;
-
-            document.getElementById("SettingsToMain").addEventListener("click", () => {
-                document.getElementById("settings").style.display = "none";
-                document.getElementById("main_menu").style.display = "block";
-                console.log("Back to main menu from settings");
-            });
-        });
 }
+
+function returnFromSettings() {
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("main_menu").style.display = "block";
+}
+
 
 //going back on stuff
 function back1() {
     document.getElementById("gameMenu").style.display = "none";
     document.getElementById("main_menu").style.display = "block"
 }
+
+const music = document.getElementById("MainMusic");
+music.volume = 0.5;
+music.play()
+
+const volumeSlider = document.getElementById("volumeControl");
+
+volumeSlider.addEventListener("input",() => {
+    music.volume =volumeSlider.value/100;
+})
